285400e9831e499325d371200a58a57f173bb325
AP2424 <68539131+AP2424@users.noreply.github.com>
2025-02-26 13:46:43 +0200

    Scripts updated + vs files added to gitignore

--- setupscript.ps1
+++ setupscript.ps1
@@ -1,2 +1,9 @@
-python manage.py makemigrations
-python manage.py migrate
+# execute after cloning
+py -m venv venv # create new virtual environment
+venv/Scripts/activate # activate virtual environment
+pip install -r requirements.txt # install dependencies
+python manage.py makemigrations # make migrations (to recreate the database)
+python manage.py migrate # apply migrations
+
+# to run the server: python manage.py runserver
+# if new libraries are added: pip freeze > requirements.txt
