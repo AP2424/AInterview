f0069f69f62c89f248f934a1a27aabd28864b2c8
AP2424 <68539131+AP2424@users.noreply.github.com>
2025-05-20 01:01:44 +0300

    fix

---
+++ main/management/commands/reset_password.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = "Reset admin password for Render"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        admin = User.objects.filter(username='admin').first()
+        if admin:
+            admin.set_password('P@ssword123$')
+            admin.save()
+            self.stdout.write(self.style.SUCCESS('✅ Password updated.'))
+        else:
+            self.stdout.write(self.style.ERROR('❌ Admin user not found.'))
