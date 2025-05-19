1728d32863d4ea3be4e8c2a5a10851e46a00a0a2
AP2424 <68539131+AP2424@users.noreply.github.com>
2025-05-19 17:45:22 +0300

    Changes for deployment

--- AInterview/settings.py
+++ AInterview/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(&5&^+_*l-s)8mf620bhno!3g5-hmhj(8q-^-8hwf(a#nm2is^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
